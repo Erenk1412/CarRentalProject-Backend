4d94ef40cbefe776d370f7fa0f4b3b78689ed618
EREN KECHAGIA <eren.kehaya@gmail.com>
2021-02-24 04:38:43 +0300

    Kodlama.io/Day12/Homework3/AOP and ValidationAspect

--- Business/Concrete/CarMenager.cs
+++ Business/Concrete/CarMenager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
@@ -20,7 +22,7 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
-       
+      [ValidationAspect(typeof(CarValidator))] 
         public IResult Add(Car car)
         {
            
---
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -0,0 +1,24 @@
+﻿using Core.Utilities.Interceptors;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Aspects.Autofac.Validation
+{
+  public class ValidationAspect:MethodInterception
+
+    {
+        private Type _validatorType;
+        public ValidationAspect(Type validatortype)
+        {
+            if (!typeof(IValidator).IsAssignableFrom(validatortype))
+            {
+                throw new System.Exception("Bu bir dogrulama sinifi degildir");
+            }
+
+            _validatorType = validatortype;
+        }
+
+    }
+}
